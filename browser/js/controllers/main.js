8a315f7d18e317c201cef2538f28c1f6412ffc10
Tiffany W <tiffany.wang777@gmail.com>
2015-11-15 16:25:47 -0500

    took out phone number, added name

--- browser/js/controllers/main.js
+++ browser/js/controllers/main.js
@@ -1,7 +1,11 @@
 app.controller('MainCtrl', function($scope, $state){
+	$scope.isShowing = true;
 	$scope.goToState = function(){
 		var whatEmotion = logVals();
-		$state.go('emotion-state', {finalEmotion: whatEmotion});
+		var name = $scope.name;
+		var userPhone = $scope.phone;
+		$scope.isShowing = false;
+		$state.go('emotion-state', {finalEmotion: whatEmotion, userName: name, phone: userPhone});
 	}
 	$scope.goHome = function(){
 		location.reload();
@@ -11,4 +15,5 @@ app.controller('MainCtrl', function($scope, $state){
 		var audio = document.getElementById("mySound");
 		audio.play();
 	}
+
 })
--- browser/js/emotion/emotion.js
+++ browser/js/emotion/emotion.js
@@ -1,6 +1,6 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('emotion-state', {
-        url: '/emotion/:finalEmotion',
+        url: '/emotion/:finalEmotion/:userName',
         templateUrl: 'js/emotion/emotion.html',
         controller: 'EmotionCtrl'
     });
@@ -9,19 +9,22 @@ app.config(function ($stateProvider) {
 
 
 app.controller('EmotionCtrl', function ($scope, $state, $stateParams, EmotionFactory) {
-	var happyText = ["So happy you're happy! Do a happy dance!", "Share your joy with someone else today!", "Hooray you're happy! Now you can pay it forward:)", "Your smile looks amazing on you! You should wear it more often:)"];
-	var sadText = ["Don't forget that you are well loved and an amazing person!", "Don't be sad! Here, answer this: what did Jay-Z call his girlfriend before he got married? A Feyonce!", "This joke might cheer you up! What's the difference between snowmen and snowladies? Snowballs!", "The night is darkest just before the dawn. Hang tough!"];
-	var angryText = ["Don't be so upset! Take a second, breath, and think calming thoughts.", "Keep calm and carry on", "Life is too short to be angry!", "Don't be mad! Do more of what makes you happy!"];
-	var surprisedText = ["Surprise! Here's a text to add to your wonderment:)", "Why are you so surprised? You're awesome!", "Sometimes what you're looking for comes when you're not looking at all! It's nice to be surprised:)", "Give someone a surprise hug!"];
 	$scope.emotion = $stateParams.finalEmotion;
+	$scope.name = $stateParams.userName;
+	var happyText = ["So happy you're happy, " + $scope.name + "! Do a happy dance!", "Share your joy with someone else today, " + $scope.name + "!", "Hooray you're happy, " + $scope.name + "! Now you can pay it forward:)", "Your smile looks amazing on you, " + $scope.name + "! You should wear it more often:)"];
+	var sadText = ["Don't forget that you are well loved and an amazing person, " + $scope.name + "!", "Don't be sad, " + $scope.name + "! Here, answer this: what did Jay-Z call his girlfriend before he got married? A Feyonce!", "This joke might cheer you up, " + $scope.name + "! What's the difference between snowmen and snowladies? Snowballs!"];
+	var angryText = ["Don't be so upset, " + $scope.name + "! Take a second, breath, and think calming thoughts.", "Keep calm and carry on, " + $scope.name + "", "Life is too short to be angry, " + $scope.name + "!", "Don't be mad, " + $scope.name + "! Do more of what makes you happy!"];
+	var surprisedText = ["Why are you so surprised, " + $scope.name + "? You're awesome!", "Sometimes what you're looking for comes when you're not looking at all! It's nice to be surprised, isn't it " + $scope.name + "?", "Give someone a surprise hug, " + $scope.name + "!"];
+	
+
 	if($scope.emotion == 'happy'){
 		EmotionFactory.sendText({body: happyText[(Math.floor(Math.random() * 3) + 1)]})
 	} else if ($scope.emotion == 'sad'){
-		EmotionFactory.sendText({body: sadText[(Math.floor(Math.random() * 3) + 1)] })
+		EmotionFactory.sendText({body: sadText[(Math.floor(Math.random() * 2) + 1)]})
 	} else if($scope.emotion == 'angry'){
-		EmotionFactory.sendText({body: angryText[(Math.floor(Math.random() * 3) + 1)] })
+		EmotionFactory.sendText({body: angryText[(Math.floor(Math.random() * 3) + 1)]})
 	} else if($scope.emotion == 'surprised') {
-		EmotionFactory.sendText({body: surprisedText[(Math.floor(Math.random() * 3) + 1)] })
+		EmotionFactory.sendText({body: surprisedText[(Math.floor(Math.random() * 2) + 1)]})
 	}
 	
 
